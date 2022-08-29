77e0a979d491dc5bd974bb2514f0a0dbee965790
samuelcpr <samuelcpr464@gmail.com>
2022-08-28 23:37:53 -0300

    feat: create routes

---
+++ src/routes.ts
@@ -0,0 +1,9 @@
+import
+
+const router = express.Router()
+router.get('/', (req, res) => res.json({ hello: 'Hello, world!' }))
+
+router.get('/candidates', async (req, res) => {
+  const candidates = await Candidate.findAll()
+  return res.json(candidates)
+})
